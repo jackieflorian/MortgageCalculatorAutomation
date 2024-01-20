aeab3b7629179975740c4fc55b77a6f075ce1798
jackieflorian <jackieflrn@gmail.com>
2024-01-20 14:00:07 -0500

    added new comment

--- MortgageCalculatorGitPractice/src/main/java/com/application/MortgageCalculatorApp.java
+++ MortgageCalculatorGitPractice/src/main/java/com/application/MortgageCalculatorApp.java
@@ -8,6 +8,7 @@ public class MortgageCalculatorApp {
 	
 	public static void main(String[] args) {
 		System.out.println("Mortgage Calculator application is starting...");
+		System.out.println("other random message i am adding for practice");
 	}
 
 }
